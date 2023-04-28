5f423ca90a37ccdb4b086eea0474792049edcb11
Oleh Abanin <oleh@outvio.com>
2023-04-28 18:40:21 +0300

    fix: test requests

--- app.ts
+++ app.ts
@@ -1,7 +1,32 @@
 import * as http from "http";
-import {requestHandler} from "./routes";
+import {requestHandler} from "./nodejs_routes";
+import express from "express";
+
+const app = express();
+import * as bodyParser from "body-parser";
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from Express!</h1>');
+});
 
 
-const server = http.createServer(requestHandler);
+
+
+
+const server = http.createServer(app);
 
 server.listen(3000);
+
